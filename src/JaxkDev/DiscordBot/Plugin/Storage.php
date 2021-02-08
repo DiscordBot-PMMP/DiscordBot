0a6c60a603fc79cdf077f65d65b43a7778a0a43b
JaxkDev <JaxkDev@gmail.com>
2021-02-08 11:02:58 +0000

    Removal methods added to storage. - To be tested.

--- src/JaxkDev/DiscordBot/Plugin/Storage.php
+++ src/JaxkDev/DiscordBot/Plugin/Storage.php
@@ -22,9 +22,9 @@ use JaxkDev\DiscordBot\Communication\Models\User;
  * Notes:
  * Dont use array_search or foreach, way too slow on large arrays.
  * (~= 10ms to search for one user in a 30k db
+ *
+ * unset() on the removes doesnt destroy the objects until all references are unset....
  */
-
-//todo, removes.
 
 class Storage{
 
@@ -66,6 +66,31 @@ class Storage{
 		self::$serverMap[$server->getId()] = $server;
 		if(!isset(self::$channelServerMap[$server->getId()])) self::$channelServerMap[$server->getId()] = [];
 		if(!isset(self::$memberServerMap[$server->getId()])) self::$memberServerMap[$server->getId()] = [];
+		if(!isset(self::$roleServerMap[$server->getId()])) self::$roleServerMap[$server->getId()] = [];
+	}
+
+	/**
+	 * NOTICE, Removes all linked members,channels and roles.
+	 * @param string $serverId
+	 */
+	public static function removeServer(string $serverId): void{
+		if(!isset(self::$serverMap[$serverId])) return; //Was never added or already deleted.
+		unset(self::$serverMap[$serverId]);
+		//Remove servers channels.
+		foreach(self::$channelServerMap[$serverId] as $cid){
+			unset(self::$channelMap[$cid]);
+		}
+		unset(self::$channelServerMap[$serverId]);
+		//Remove servers members.
+		foreach(self::$memberServerMap[$serverId] as $mid){
+			unset(self::$memberMap[$mid]);
+		}
+		unset(self::$memberServerMap[$serverId]);
+		//Remove servers roles.
+		foreach(self::$roleServerMap[$serverId] as $rid){
+			unset(self::$roleMap[$rid]);
+		}
+		unset(self::$roleServerMap[$serverId]);
 	}
 
 	public static function getChannel(string $id): ?Channel{
@@ -90,6 +115,16 @@ class Storage{
 		self::$channelMap[$channel->getId()] = $channel;
 	}
 
+	public static function removeChannel(string $channelId): void{
+		$channel = self::getChannel($channelId);
+		if($channel === null) return; //Already deleted or not added.
+		$serverId = $channel->getServerId();
+		unset(self::$channelMap[$channelId]);
+		$i = array_search($channelId, self::$channelServerMap[$serverId], true);
+		if($i === false || is_string($i)) return; //Not in this servers channel map.
+		array_splice(self::$channelServerMap[$serverId], $i, 1);
+	}
+
 	public static function getMember(string $id): ?Member{
 		return self::$memberMap[$id] ?? null;
 	}
@@ -112,12 +147,26 @@ class Storage{
 		self::$memberMap[$member->getId()] = $member;
 	}
 
+	public static function removeMember(string $memberID): void{
+		$member = self::getMember($memberID);
+		if($member === null) return; //Already deleted or not added.
+		$serverId = $member->getServerId();
+		unset(self::$memberMap[$memberID]);
+		$i = array_search($memberID, self::$memberServerMap[$serverId], true);
+		if($i === false || is_string($i)) return; //Not in this servers member map.
+		array_splice(self::$memberServerMap[$serverId], $i, 1);
+	}
+
 	public static function getUser(string $id): ?User{
 		return self::$userMap[$id] ?? null;
 	}
 
 	public static function addUser(User $user): void{
 		self::$userMap[$user->getId()] = $user;
+	}
+
+	public static function removeUser(string $userId): void{
+		unset(self::$userMap[$userId]);
 	}
 
 	public static function getRole(string $id): ?Role{
@@ -140,6 +189,16 @@ class Storage{
 	public static function addRole(Role $role): void{
 		self::$roleServerMap[$role->getServerId()][] = $role->getId();
 		self::$roleMap[$role->getId()] = $role;
+	}
+
+	public static function removeRole(string $roleID): void{
+		$role = self::getRole($roleID);
+		if($role === null) return; //Already deleted or not added.
+		$serverId = $role->getServerId();
+		unset(self::$roleMap[$roleID]);
+		$i = array_search($roleID, self::$roleServerMap[$serverId], true);
+		if($i === false || is_string($i)) return; //Not in this servers role map.
+		array_splice(self::$roleServerMap[$serverId], $i, 1);
 	}
 
 	public static function getBotUser(): ?User{
