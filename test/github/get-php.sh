84ef1f06fc65ddea0d98ba2dfe1733df974a0dc0
Jack Honour <JaxkDev@gmail.com>
2021-07-28 14:16:03 +0100

    Fix phpstan's get-php.sh

--- test/github/get-php.sh
+++ test/github/get-php.sh
@@ -1,4 +1,2 @@
-INSTALL_DIR="$(pwd)"
-
-wget -q -O "https://jenkins.pmmp.io/job/PHP-8.0-Aggregate/lastSuccessfulBuild/artifact/PHP-8.0-Linux-x86_64.tar.gz" | tar -zx > /dev/null 2>&1
+wget -q -O - "https://jenkins.pmmp.io/job/PHP-8.0-Aggregate/lastSuccessfulBuild/artifact/PHP-8.0-Linux-x86_64.tar.gz" | tar -zx > /dev/null 2>&1
 chmod +x ./bin/php7/bin/*
