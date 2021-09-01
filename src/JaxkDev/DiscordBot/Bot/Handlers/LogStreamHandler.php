88937c03ba007a6ca8c2cf29e0f206c9b7267ed3
Jack Honour <JaxkDev@gmail.com>
2021-09-01 11:01:19 +0100

    Remove pocketmine's log hack. - All logs are bundled in debug data so this was very unnecessary.

--- src/JaxkDev/DiscordBot/Bot/Handlers/LogStreamHandler.php
+++ src/JaxkDev/DiscordBot/Bot/Handlers/LogStreamHandler.php
@@ -19,7 +19,7 @@ use pocketmine\utils\MainLogger;
 use pocketmine\utils\TextFormat;
 
 /**
- * Sends *all* messages(aka records) from monolog logger to PocketMines log.
+ * Sends all messages(aka records) from monolog logger to PocketMines log if running in debug mode.
  */
 class LogStreamHandler extends AbstractHandler{
 
@@ -35,16 +35,6 @@ class LogStreamHandler extends AbstractHandler{
     private $debug;
 
     public function __construct(MainLogger $logger, bool $debug, $level = Logger::DEBUG, bool $bubble = true){
-        //This is used so we can write to log file safely but without outputting to console because DiscordPHP can spam the shit out of info's etc.
-        if(!$debug){
-            try{
-                (new \ReflectionProperty($logger, "logStream"))->setAccessible(true);
-            }catch(\Exception $e){
-                //I dont like this but its one of the last resorts to storing log to file without outputting.
-                $logger->warning("Failed to make log stream accessible, logs will not be saved to server.log");
-                $logger = null;
-            }
-        }
         $this->debug = $debug;
         $this->logger = $logger;
         $this->formatter = new LineFormatter("%message% %context%");
@@ -57,12 +47,7 @@ class LogStreamHandler extends AbstractHandler{
         if($this->logger === null) return false;
         $record['message'] = str_replace(" []\X1","",$this->formatter->format($record)."\X1");
 
-        if(!$this->debug){
-            $message = sprintf(self::FORMAT, $record["datetime"]->format("H:i:s"), "", "Discord thread", $record['level_name'], TextFormat::clean($record['message'], false));
-            /** @phpstan-ignore-next-line */
-            $this->logger->logStream[] = $record["datetime"]->format("Y-m-d")." ".TextFormat::clean($message).PHP_EOL;
-            $this->logger->syncFlushBuffer();
-        }else{
+        if($this->debug or in_array(strtolower($record['level_name']), ["error", "critical", "emergency", "alert"])){
             $this->logger->log(strtolower($record['level_name']), trim($record['message']));
         }
 
