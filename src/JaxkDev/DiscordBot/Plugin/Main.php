22fe0bd6d2ce8d3b0e288806a2202b3c7cb8860a
Jack Honour <JaxkDev@gmail.com>
2021-10-02 13:47:16 +0100

    Change copyright end year to present. - Save time and effort in the future. - [ci skip]

--- src/JaxkDev/DiscordBot/Plugin/Main.php
+++ src/JaxkDev/DiscordBot/Plugin/Main.php
@@ -3,7 +3,7 @@
  * DiscordBot, PocketMine-MP Plugin.
  *
  * Licensed under the Open Software License version 3.0 (OSL-3.0)
- * Copyright (C) 2020-2021 JaxkDev
+ * Copyright (C) 2020-present JaxkDev
  *
  * Twitter :: @JaxkDev
  * Discord :: JaxkDev#2698
@@ -183,7 +183,7 @@ class Main extends PluginBase{
  * DiscordBot, PocketMine-MP Plugin.
  *
  * Licensed under the Open Software License version 3.0 (OSL-3.0)
- * Copyright (C) 2020-2021 JaxkDev
+ * Copyright (C) 2020-present JaxkDev
  *
  * Twitter :: @JaxkDev
  * Discord :: JaxkDev#2698
