f7e4c67ac4d06fd71fb6d215509321659668cb49
Jack Honour <JaxkDev@gmail.com>
2021-11-02 14:45:30 +0000

    Notices added to DiscordReady event.

--- src/Plugin/Events/DiscordReady.php
+++ src/Plugin/Events/DiscordReady.php
@@ -13,7 +13,11 @@
 namespace JaxkDev\DiscordBot\Plugin\Events;
 
 /**
- * DiscordBot has connected and we are now in contact with discord.
+ * DiscordBot has connected, and we are now in contact with discord.
+ *
+ * Storage has been populated with initial data but bans + invites may not yet be added,
+ * suggest waiting a few seconds before using bans/invite data if any.
+ *
  * @see DiscordClosed Emitted when DiscordBot disconnects.
  */
 class DiscordReady extends DiscordBotEvent{}
