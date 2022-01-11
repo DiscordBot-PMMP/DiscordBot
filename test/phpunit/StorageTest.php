5a09cc11ae6c1187b58548a5560333a2adc45be9
Jack Honour <JaxkDev@gmail.com>
2022-01-11 20:58:20 +0000

    Start Storage Unit tests.

---
+++ test/phpunit/StorageTest.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * DiscordBot, PocketMine-MP Plugin.
+ *
+ * Licensed under the Open Software License version 3.0 (OSL-3.0)
+ * Copyright (C) 2020-present JaxkDev
+ *
+ * Twitter :: @JaxkDev
+ * Discord :: JaxkDev#2698
+ * Email   :: JaxkDev@gmail.com
+ */
+
+declare(strict_types=1);
+
+use JaxkDev\DiscordBot\Models\Server;
+use JaxkDev\DiscordBot\Plugin\Storage;
+use PHPUnit\Framework\TestCase;
+
+//TODO Add model constructor dependencies where applicable.
+//TODO Better model usage.
+final class StorageTest extends TestCase{
+
+    public function testGetServers(): void{
+        $this->assertEmpty(Storage::getServers(), "If this triggers, the plugin has died :)");
+    }
+
+    /**
+     * //TODO Depends on Server Creation.
+     * @depends testGetServers
+     */
+    public function testAddServer(): void{
+        $s = new Server("554059221847638040", "Test Server", "UK", "282819886198030336", false, 10);
+        Storage::addServer($s);
+        $this->assertCount(1, Storage::getServers(), "Failed to add server to storage.");
+        //Duplicated.
+        Storage::addServer($s);
+        $this->assertCount(1, Storage::getServers(), "Failed to detect duplicated server being added to storage.");
+    }
+
+    /**
+     * //TODO Depends on Server Creation & Server::setRegion
+     * @depends testAddServer
+     */
+    public function testUpdateServer(): void{
+        $s = new Server("554059221847638040", "Test Server", "UK", "282819886198030336", false, 10);
+        $s->setRegion("US");
+        Storage::updateServer($s);
+        $this->assertCount(1, Storage::getServers());
+        //Different ID, should be added not updated.
+        $s = new Server("554059221847638041", "Test Server 2", "UK", "282819886198030336", false, 20);
+        Storage::updateServer($s);
+        $this->assertCount(2, Storage::getServers());
+    }
+
+    /**
+     * @depends testGetServers
+     * @depends testUpdateServer
+     */
+    public function testGetServer(): void{
+        $this->assertInstanceOf(Server::class, Storage::getServer("554059221847638040"));
+        $this->assertInstanceOf(Server::class, Storage::getServer("554059221847638041"));
+        $this->assertNull(Storage::getServer("111111111111111111"));
+        $this->assertNull(Storage::getServer("Invalid ID"));
+    }
+}
