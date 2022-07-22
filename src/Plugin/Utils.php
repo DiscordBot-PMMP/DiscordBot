5992231c003f59dda518e939a5275278229b8875
Jack Honour <JaxkDev@gmail.com>
2022-07-22 19:29:42 +0100

    Adjusted discord snowflake check. - Length can be 19 now.

--- src/Plugin/Utils.php
+++ src/Plugin/Utils.php
@@ -21,7 +21,7 @@ abstract class Utils{
     /** Checks a discord snowflake by verifying the timestamp at when it was created. */
     public static function validDiscordSnowflake(string $snowflake): bool{
         $len = strlen($snowflake);
-        if($len < 17 or $len > 18) return false;
+        if($len < 17 or $len > 19) return false;
         $timestamp = self::getDiscordSnowflakeTimestamp($snowflake);
         if($timestamp > time()+86400 or $timestamp <= 1420070400) return false; //+86400 (24h for any timezone problems)
         return true;
