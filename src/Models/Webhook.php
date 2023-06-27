5c335824406a7ec84a81da6ca836e4269f19c342
Jack Honour <JaxkDev@gmail.com>
2023-06-27 16:30:53 +0100

    Webhook Model updated.

--- src/Models/Webhook.php
+++ src/Models/Webhook.php
@@ -14,42 +14,81 @@ namespace JaxkDev\DiscordBot\Models;
 
 use JaxkDev\DiscordBot\Plugin\Utils;
 
+/**
+ * @link https://discord.com/developers/docs/resources/webhook#webhook-object
+ */
 class Webhook{
 
-    //TODO Enums
-    const TYPE_NORMAL = 1, //Standard webhook
-        TYPE_FOLLOWER = 2; //Receiving 'news' from another channel.
+    /**
+     * The type of the webhook
+     * @see WebhookType
+     */
+    private WebhookType $type;
 
-    /** Only present on created webhooks. */
+    /** The ID of the webhook */
     private ?string $id;
 
-    /**
-     * @see Webhook::TYPE_NORMAL
-     * @see Webhook::TYPE_FOLLOWER
-     */
-    private int $type;
+    /** The guild ID this webhook is for, if any */
+    private ?string $guild_id;
 
-    /** User that added/created this webhook, only present on created webhooks. */
+    /** The channel ID this webhook is for, if any */
+    private ?string $channel_id;
+
+    /** the user this webhook was created by */
     private ?string $user_id;
 
-    private string $channel_id;
+    /**
+     * The name of the webhook.
+     * Cannot contain the substrings 'clyde' or 'discord' (case-insensitive), limit 80 characters
+     */
+    private ?string $name;
 
-    private string $name;
-
+    /** The user avatar hash of the webhook */
     private ?string $avatar;
 
-    /** Only present on TYPE_NORMAL. */
+    /** The secure token of the webhook (only for Incoming Webhooks) */
     private ?string $token;
 
-    public function __construct(int $type, string $channel_id, string $name, ?string $id = null, ?string $user_id = null,
-                                ?string $avatar = null, ?string $token = null){
-        $this->setId($id);
+    /** The bot/OAuth2 application that created this webhook */
+    private ?string $application_id;
+
+    /** The guild ID of the channel that this webhook is following (only for Channel Follower Webhooks) */
+    private ?string $source_guild_id;
+
+    /** The channel ID of the channel that this webhook is following (only for Channel Follower Webhooks) */
+    private ?string $source_channel_id;
+
+    /**
+     * Create a base webhook model with only required fields, ready for creation.
+     *
+     * @see Api::createWebhook()
+     */
+    public static function create(string $channel_id, string $name): self{
+        return new self(WebhookType::INCOMING, channel_id: $channel_id, name: $name);
+    }
+
+    public function __construct(WebhookType $type, ?string $id = null, ?string $guild_id = null, ?string $channel_id = null,
+                                ?string $user_id = null, ?string $name = null, ?string $avatar = null, ?string $token = null,
+                                ?string $application_id = null, ?string $source_guild_id = null, ?string $source_channel_id = null){
         $this->setType($type);
-        $this->setUserId($user_id);
+        $this->setId($id);
+        $this->setGuildId($guild_id);
         $this->setChannelId($channel_id);
+        $this->setUserId($user_id);
         $this->setName($name);
         $this->setAvatar($avatar);
         $this->setToken($token);
+        $this->setApplicationId($application_id);
+        $this->setSourceGuildId($source_guild_id);
+        $this->setSourceChannelId($source_channel_id);
+    }
+
+    public function getType(): WebhookType{
+        return $this->type;
+    }
+
+    public function setType(WebhookType $type): void{
+        $this->type = $type;
     }
 
     public function getId(): ?string{
@@ -63,26 +102,15 @@ class Webhook{
         $this->id = $id;
     }
 
-    public function getUserId(): ?string{
-        return $this->user_id;
+    public function getGuildId(): ?string{
+        return $this->guild_id;
     }
 
-    public function setUserId(?string $user_id): void{
-        if($user_id !== null and !Utils::validDiscordSnowflake($user_id)){
-            throw new \AssertionError("User ID '$user_id' is invalid.");
+    public function setGuildId(?string $guild_id): void{
+        if($guild_id !== null and !Utils::validDiscordSnowflake($guild_id)){
+            throw new \AssertionError("Guild ID '$guild_id' is invalid.");
         }
-        $this->user_id = $user_id;
-    }
-
-    public function getType(): int{
-        return $this->type;
-    }
-
-    public function setType(int $type): void{
-        if($type > self::TYPE_FOLLOWER or $type < self::TYPE_NORMAL){
-            throw new \AssertionError("Type '$type' is invalid.");
-        }
-        $this->type = $type;
+        $this->guild_id = $guild_id;
     }
 
     public function getChannelId(): string{
@@ -96,11 +124,28 @@ class Webhook{
         $this->channel_id = $channel_id;
     }
 
+    public function getUserId(): ?string{
+        return $this->user_id;
+    }
+
+    public function setUserId(?string $user_id): void{
+        if($user_id !== null and !Utils::validDiscordSnowflake($user_id)){
+            throw new \AssertionError("User ID '$user_id' is invalid.");
+        }
+        $this->user_id = $user_id;
+    }
+
     public function getName(): string{
         return $this->name;
     }
 
     public function setName(string $name): void{
+        if(strlen($name) > 80){
+            throw new \AssertionError("Name '$name' is too long, max 80 characters.");
+        }
+        if(stripos($name, 'clyde') !== false or stripos($name, 'discord') !== false){
+            throw new \AssertionError("Name '$name' is not allowed, cannot contain 'clyde' or 'discord'.");
+        }
         $this->name = $name;
     }
 
@@ -120,29 +165,77 @@ class Webhook{
         $this->token = $token;
     }
 
+    public function getApplicationId(): ?string{
+        return $this->application_id;
+    }
+
+    public function setApplicationId(?string $application_id): void{
+        if($application_id !== null and !Utils::validDiscordSnowflake($application_id)){
+            throw new \AssertionError("Application ID '$application_id' is invalid.");
+        }
+        $this->application_id = $application_id;
+    }
+
+    public function getSourceGuildId(): ?string{
+        return $this->source_guild_id;
+    }
+
+    public function setSourceGuildId(?string $source_guild_id): void{
+        if($source_guild_id !== null and !Utils::validDiscordSnowflake($source_guild_id)){
+            throw new \AssertionError("Source Guild ID '$source_guild_id' is invalid.");
+        }
+        $this->source_guild_id = $source_guild_id;
+    }
+
+    public function getSourceChannelId(): ?string{
+        return $this->source_channel_id;
+    }
+
+    public function setSourceChannelId(?string $source_channel_id): void{
+        if($source_channel_id !== null and !Utils::validDiscordSnowflake($source_channel_id)){
+            throw new \AssertionError("Source Channel ID '$source_channel_id' is invalid.");
+        }
+        $this->source_channel_id = $source_channel_id;
+    }
+
+    public function getURL(): ?string{
+        if($this->id === null or $this->token === null){
+            return null;
+        }
+        return "https://discord.com/api/webhooks/{$this->id}/{$this->token}";
+    }
+
     //----- Serialization -----//
 
     public function __serialize(): array{
         return [
-            $this->id,
             $this->type,
-            $this->user_id,
+            $this->id,
+            $this->guild_id,
             $this->channel_id,
+            $this->user_id,
             $this->name,
             $this->avatar,
-            $this->token
+            $this->token,
+            $this->application_id,
+            $this->source_guild_id,
+            $this->source_channel_id
         ];
     }
 
     public function __unserialize(array $data): void{
         [
-            $this->id,
             $this->type,
-            $this->user_id,
+            $this->id,
+            $this->guild_id,
             $this->channel_id,
+            $this->user_id,
             $this->name,
             $this->avatar,
-            $this->token
+            $this->token,
+            $this->application_id,
+            $this->source_guild_id,
+            $this->source_channel_id
         ] = $data;
     }
 }
